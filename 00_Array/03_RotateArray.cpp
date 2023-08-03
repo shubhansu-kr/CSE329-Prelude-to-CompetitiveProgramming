16d5756c68f52fe430e973c71fc46d1dccf9bd60
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-03 14:01:03 +0530

    Solultion4: Rotate left
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Array/03_RotateArray.cpp
+++ 00_Array/03_RotateArray.cpp
@@ -3,6 +3,47 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution4 {
+public:
+    // Rotate Anti Clockwise | Rotate left (Approach2)
+
+    // Time complexity: O(N)
+    // Space Complexity: O(1)
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k %= n;
+
+        // Divide the array into two parts 
+        // 0 to K-1 | K To N-1 
+        // Reverse both the parts 
+        // Reverse the whole array
+
+        for (int i = 0; i < k/2; ++i)
+        {
+            swap(nums[i], nums[k-i-1]);
+        }
+
+        for (int i = 0; i < (n-k)/2; i++)
+        {
+            swap(nums[k+i], nums[n-1-i]);
+        }
+            
+        for (int i = 0; i < n/2; ++i)
+        {
+            swap(nums[i], nums[n-i-1]);
+        }
+
+        // 1, 2, 3, 4, 5, 6, 7  -> 4, 5, 6, 7, 1, 2, 3
+        // n = 7  |  k = 3
+
+        // 1, 2, 3 | 4, 5, 6, 7                  Divide
+        // 3, 2, 1 | 7, 6, 5, 4                  Reverse Both part
+        // 4, 5, 6, 7, 1, 2, 3                   Reverse the whole array
+    }
+};
+
 class Solution3 {
 public:
     // Rotate Anti Clockwise | Rotate left (Approach1)
