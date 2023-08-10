19983ed33050c1df2ddeee9bf3407ded710d5cb9
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-10 12:56:21 +0530

    Solution1: O(n) TC
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Array/08_NiceSubArray.cpp
+++ 00_Array/08_NiceSubArray.cpp
@@ -3,7 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = 0;
+        
+        vector<int> oddIndex;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] % 2) oddIndex.emplace_back(i);
+        }
+        
+        for (int i = k-1; i < oddIndex.size(); ++i)
+        {
+            int b = oddIndex[i], a = oddIndex[i-k+1];
+            
+            int p = a,  q = n-b-1;
+            if (i < oddIndex.size()-1) q = oddIndex[i+1] - b - 1; 
+            if (i-k >= 0) p = a - oddIndex[i-k] -1;
+
+            ans += ((p*q) + (p+q) + 1);
+        }
+        
+        return ans;
+    }
+};
+
 class Solution {
+    // BruteForce: Sliding window with variable size.
     // Time Limit Exceeded
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
