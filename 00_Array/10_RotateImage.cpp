b5173e7f35f5230a7544091b522c65215dceb289
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-14 08:55:13 +0530

    Solution2: Summary
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Array/10_RotateImage.cpp
+++ 00_Array/10_RotateImage.cpp
@@ -3,6 +3,88 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution2 {
+    // Summary: 
+public: 
+    void rotateClockwisePrimeDiagonal(vector<vector<int>>& matrix) {
+        // Inputs: 
+        int n = matrix.size();
+        
+        // Solution: 
+        reverse(matrix.begin(), matrix.end());
+
+        // swap along the primary diagonal.
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }            
+        }
+
+        // Reverse then swap
+    }
+
+    void rotateClockwiseSecondryDiagonal(vector<vector<int>>& matrix) {
+        // Inputs: 
+        int n = matrix.size();
+        
+        // Solution: 
+
+        // swap the along the secondry diagonal 
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n-1-i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }            
+        }
+
+        reverse(matrix.begin(), matrix.end());
+
+        // Swap Then Reverse.
+    }
+    
+    void rotateAntiClockwisePrimeDiagonal(vector<vector<int>>& matrix) {
+        // Inputs: 
+        int n = matrix.size();
+        
+        // Solution: 
+
+        // swap along the primary diagonal.
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }            
+        }
+
+        reverse(matrix.begin(), matrix.end());
+        
+        // Swap then reverse.
+    }
+
+    void rotateAntiClockwiseSecondryDiagonal(vector<vector<int>>& matrix) {
+        // Inputs: 
+        int n = matrix.size();
+        
+        // Solution: 
+        reverse(matrix.begin(), matrix.end());
+
+        // swap the along the secondry diagonal 
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n-1-i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }            
+        }
+
+        // Reverse then swap
+    }
+};
+
 class Solution1 {
     // To rotate the matrix clockwise
 
@@ -19,7 +101,7 @@ class Solution1 {
     // or we can change the diagonal and we have to change the order of steps.
     // reverse the array -> swap the symmetry along diagonal(0, n-1 to n-1, 0)  
     // will result in anti-clockwise rotation.
-    
+
 public:
     void rotate(vector<vector<int>>& matrix) {
         // Inputs: 
