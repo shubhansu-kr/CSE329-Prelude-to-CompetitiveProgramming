3710fd96aedfceaf2db202c4b6a8e637976b3377
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-14 08:46:33 +0530

    Solution1: Rotation using swap
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Array/10_RotateImage.cpp
+++ 00_Array/10_RotateImage.cpp
@@ -3,6 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // To rotate the matrix clockwise
+
+    // Reverse the array -> swap the symmetry along diagonal
+    // diagonal(0,0 to n-1,n-1);
+
+    // or we can change the diagonal and we have to change the order of steps.
+    // swap the symmetry along diagonal(0, n-1 to n-1, 0) -> reverse the array, 
+    // will result in clockwise rotation.
+
+    // To rotate the matrix anticlockwise
+    // Swap the symmetry along diagonal(primary) -> reverse the array.
+
+    // or we can change the diagonal and we have to change the order of steps.
+    // reverse the array -> swap the symmetry along diagonal(0, n-1 to n-1, 0)  
+    // will result in anti-clockwise rotation.
+    
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // Inputs: 
+        int n = matrix.size();
+        
+        // Solution: 
+        reverse(matrix.begin(), matrix.end());
+
+        // swap the symmetry 
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }            
+        }
+    }
+};
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
