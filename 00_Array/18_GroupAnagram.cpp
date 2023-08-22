7f03e0add4f204c6bbd6630269738e1e58b1bf6b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-22 08:56:38 +0530

    Group anagram togethers
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 00_Array/18_GroupAnagram.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/group-anagrams/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // BruteForce Solution: sort
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(auto &it: strs) {
+            string str = it;
+            sort(str.begin(), str.end());
+            mp[str].emplace_back(it);
+        }
+        vector<vector<string>> ans;
+        for(auto &it: mp){
+            ans.emplace_back(it.second);
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
