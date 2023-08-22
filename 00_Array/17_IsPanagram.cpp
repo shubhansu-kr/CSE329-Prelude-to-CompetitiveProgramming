5ba29bec16be16cb404aed92b1f3105b30086c46
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-22 08:20:48 +0530

    Solution1
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 00_Array/17_IsPanagram.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution1 {
+public:
+    bool checkIfPangram(string sentence) {
+        map<char, int> freq;
+        for(auto &it: sentence) {
+            freq[it]++;
+        }
+        for(auto &it: freq){
+            if (it.second == 0) return false;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> freq(26, 0);
+        for(auto &it: sentence) {
+            freq[it-'a']++;
+        }
+        for(auto &it: freq){
+            if (!it) return false;
+        }
+        return true;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
