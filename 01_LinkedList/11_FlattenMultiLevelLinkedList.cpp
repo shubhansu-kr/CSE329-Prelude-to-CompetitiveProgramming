629227d5de3603cc807ee8abd4e306d5da7f974b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-04 09:12:48 +0530

    Solution1: Iterative approach
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_LinkedList/11_FlattenMultiLevelLinkedList.cpp
+++ 01_LinkedList/11_FlattenMultiLevelLinkedList.cpp
@@ -13,9 +13,9 @@ public:
     Node* child;
 };
 
-class Solution {
+class Solution1 {
+    // Iterative Solution
 public:
-
     Node* solve(Node *parent) {
         Node *p = parent, *prev = nullptr;
         while(p) {
@@ -29,6 +29,57 @@ public:
 
                 tail->next = temp;
                 if (temp) temp->prev = tail;
+
+                p = tail ;
+            }
+            prev = p;
+            p = p->next;
+        }
+        return prev;
+    }
+
+    Node* flatten(Node* head) {
+        Node *p = head, *temp;
+        while(p) {
+            if (p->child){
+                temp = p->next;
+
+                Node * q = p->child;
+
+                p->next = q;
+                q->prev = p;
+
+                while(q->next) {
+                    q = q->next;
+                }
+
+                q->next = temp;
+                if (temp) temp->prev = q;
+            }
+            p = p->next;
+        }
+        return head;
+    }
+};
+
+class Solution {
+    // Recursive Solution
+public:
+    Node* solve(Node *parent) {
+        Node *p = parent, *prev = nullptr;
+        while(p) {
+            if (p->child) {
+                Node *tail = solve(p->child);
+                Node *temp = p->next;
+
+                p->next = p->child;
+                p->child->prev = p;
+                p->child = nullptr;
+
+                tail->next = temp;
+                if (temp) temp->prev = tail;
+
+                p = tail ;
             }
             prev = p;
             p = p->next;
