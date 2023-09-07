949c65014bff799f724c3fa71fd322d62775bbe8
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-07 08:27:02 +0530

    Incomplete
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_Maths/00_SuperPow.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/super-pow/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int superPow(int a, vector<int>& b) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
