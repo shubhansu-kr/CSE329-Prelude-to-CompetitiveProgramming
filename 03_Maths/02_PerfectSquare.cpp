f332367ae6abb027dc7b9630affbe94d391de3df
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-12 08:47:19 +0530

    Memoization
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 03_Maths/02_PerfectSquare.cpp
+++ 03_Maths/02_PerfectSquare.cpp
@@ -3,6 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Recursion: Memoization
+public:
+    int solve(vector<vector<int>> &dp, int n, int i) {
+        if (n == 0) return 0;
+        if (n < i * i) return 10e5;
+
+        if (dp[n][i] != -1) return dp[n][i];
+
+        int pick = 1 + solve(dp, n-i*i, i);
+        int noPick = solve(dp, n, i+1);
+        
+        return dp[n][i] = min(pick, noPick);
+    }
+
+    int numSquares(int n) {
+        vector<vector<int>> dp(n+1, vector<int>((sqrt(n)+1), -1));
+        return solve(dp, n, 1);
+    }
+};
+
 class Solution {
     // BruteForce: Recursion
 public:
