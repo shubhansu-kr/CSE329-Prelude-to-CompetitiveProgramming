89f4a9a72379eb67293761be1180ce96cd358aef
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-14 09:05:03 +0530

    Euler Totient
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_Maths/07_EulerTotient.cpp
@@ -0,0 +1,33 @@
+// https://practice.geeksforgeeks.org/problems/euler-totient-function4604/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // function to find totient of n
+    long long ETF(long long N)
+    {
+        // code here
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.ETF(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
