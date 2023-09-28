e2057b50a5ab2a655b67c0d5688c32a6c7b5c063
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-28 08:40:57 +0530

    GCD
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 03_Maths/08_GCD.cpp
+++ 03_Maths/08_GCD.cpp
@@ -3,7 +3,43 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Extended Euclidian Algo: 
+
+    // aX + bY = gcd(a, b)
+    // gcd(b, a%b) = gcd(a, b);
+    // bX1 + (a%b)Y1 = aX + bY;
+
+    // p % q = p - p/q * q; (Modulus equivalent).
+    
+    // Solve the eq: 
+    // bX1 + (a%b)Y1 = aX + bY;
+    // bX1 + (a - a/b * b) Y1 = aX + bY;
+    // bX1 + aY1 - (a/b * b)Y1 = aX + bY;
+    // aY1 + b(X1 - (a/b)*Y1) = aX + bY;
+    
+public: 
+    int extended_gcd(int a,int b,int *x,int *y)
+    {
+        if(a==0)
+        {
+            *x=0;
+            *y=1;
+            return b;
+        }
+        
+        int x1,y1;
+        int gcd=extended_gcd(b%a,a,&x1,&y1);
+    
+        *x=y1-b/a*x1;
+        *y=x1;
+        
+        return gcd;
+    }
+};
+
 class Solution {
+    // Euclidian Algo
 public: 
     int gcd(int &a, int &b){
         if (a < b) {
