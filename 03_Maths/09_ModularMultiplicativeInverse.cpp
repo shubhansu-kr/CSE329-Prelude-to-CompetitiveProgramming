c67a3f8ff73ebdaef918e3899d680bab642a2b2b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-03 08:47:07 +0530

    Incomplete
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_Maths/09_ModularMultiplicativeInverse.cpp
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/modular-multiplicative-inverse-1587115620/1
+
+
+#include<iostream>
+using namespace std;
+
+
+class Solution{
+    public:
+    int modInverse(int a, int m)
+    {
+        
+    }
+};
+
+int main()
+{
+	int T;
+	
+	cin>>T;
+	while(T--){
+		int a,m;
+		
+		cin>>a>>m;
+		Solution ob;
+		
+		cout << ob.modInverse(a, m)<<endl;
+	}
+    return 0;
+}
